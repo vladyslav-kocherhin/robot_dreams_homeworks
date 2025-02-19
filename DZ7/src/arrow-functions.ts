ea8e274a5cea692daa735d87c5cb36b0d54d7da0
v.kocherhin <vladyslav.kocherhin@gmail.com>
2025-02-20 00:56:08 +0200

    Hometask 7 - updated

--- DZ7/src/arrow-functions.ts
+++ DZ7/src/arrow-functions.ts
@@ -1,19 +1,28 @@
-// Стрілкова функція, яка приймає масив чисел і повертає суму його елементів
-
 const newSumArray = (arr: (number | string)[]): void => {
     let sum: number = 0;
-    for (let i = 0; i < arr.length; i++) {
-        sum += Number(arr[i]); 
-    }
-    console.log(sum);
+    let text: string = "";
+    let isNumberArray: boolean = true;
+
+    arr.forEach(item => {
+        if (typeof item === "number") {
+            sum += item;
+        } else {
+            text += item;
+            isNumberArray = false; 
+        }
+    });
+
+    isNumberArray 
+        ? console.log("Сума чисел:", sum) 
+        : console.log("Об'єднаний текст:", text);
 };
 
 // Створення двох масивів
 
-const newNumbersArray: number[] = [10, 20, 30, 40];
-const newStringsArray: string[] = ["10", "20", "30", "40"];
+const newNumbersArray = [10, 20, 30, 40];
+const newStringsArray = ["10", "20", "30", "40"];
 
 // Виклик функцій
 
 newSumArray(newNumbersArray);
-newSumArray(newStringsArray); 
+//newSumArray(newStringsArray); 
--- DZ7/src/functions.ts
+++ DZ7/src/functions.ts
@@ -1,13 +1,72 @@
 function sumArray(arr: (number | string)[]): void {
     let sum: number = 0;
-    for (let i = 0; i < arr.length; i++) {
-        sum += Number(arr[i]); 
+    let text: string = "";
+    let isNumberArray: boolean = true;
+
+    for (const item of arr) {
+        if (typeof item === "number") {
+            sum += item;
+        } else {
+            text += item;
+            isNumberArray = false; 
+        }
     }
-    console.log(sum);
+
+    if (isNumberArray) {
+        console.log("Сума чисел:", sum);
+    } else {
+        console.log("Об'єднаний текст:", text);
+    }
 }
 
-const numbersArray: number[] = [10, 20, 30, 40];
-const stringsArray: string[] = ["10", "20", "30", "40"];
+const numbersArray = [10, 20, 30, 40];
+const stringsArray = ["10", "20", "30", "40"];
 
 sumArray(numbersArray); 
-sumArray(stringsArray); 
+//sumArray(stringsArray)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function sumArray(arr: (number | string)[]): void {
+    let sum: number = 0;
+    let text: string = "";
+
+    for (const item of arr) {
+        if (typeof item === "number") {
+            sum += item; 
+        } else {
+            text += item; 
+        }
+    };
+
+    
+        if (typeof arr === "number") {
+            console.log("Сума чисел:", sum);
+        } else {
+            console.log("Об'єднаний текст:", text);
+        }
+    };
+
+
+const numbersArray = [10, 20, 30, 40];
+const stringsArray = ["10", "20", "30", "40"];
+
+sumArray(numbersArray); 
+//sumArray(stringsArray);*/
