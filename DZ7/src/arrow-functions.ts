a6968c0baa7a2294649aa3a6660cdbf6af62cca3
v.kocherhin <vladyslav.kocherhin@gmail.com>
2025-02-22 13:49:28 +0200

    Hometask 7 - Update 4

--- DZ7/src/arrow-functions.ts
+++ DZ7/src/arrow-functions.ts
@@ -1,18 +1,18 @@
 const newSumArray = (arr: number[] | string[]): void => {
     if (arr.length === 0) {
-        console.log("Масив порожній.");
+        console.log('Масив порожній.');
         return;
     }
 
-    typeof arr[0] === "number"
-        ? console.log("Сума чисел:", (arr as number[]).reduce((acc, num) => acc + num, 0))
-        : console.log("Об'єднаний текст:", (arr as string[]).join(""));
+    typeof arr[0] === 'number'
+        ? console.log('Сума чисел:', (arr as number[]).reduce((acc, num) => acc + num, 0))
+        : console.log('Об\'єднаний текст:', (arr as string[]).join(''));
 };
 
 // Створення двох масивів
 
 const newNumbersArray = [10, 20, 30, 40];
-const newStringsArray = ["10", "20", "30", "40"];
+const newStringsArray = ['10', '20', '30', '40'];
 
 // Виклик функцій
 
